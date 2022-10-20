5971b02ab4111765d6c199fc1fcb14673d465aca
Ayan George <ayan@ayan.net>
2022-10-20 10:30:40 -0400

    make getinfo() more robust.

--- prompt.go
+++ prompt.go
@@ -1,8 +1,6 @@
 /*
 
-Prompt is a very simple prompt decorator taylor made to meet my preferences.
-
-At the moment it only shows the current Git branch.
+Prompt is a really simple prompt decorator taylor made to meet my preferences.
 
 */
 
@@ -17,58 +15,39 @@ import (
 )
 
 // gitinfo writes the git branch to the provided writer.
-func gitinfo(w io.Writer) error {
+func gitinfo(w io.Writer) {
 	wd, err := os.Getwd()
 	if err != nil {
-		return err
+		return
 	}
 
-	// iterate over paths until we either reach the root or find a .git
-	// directory.
-	findGit := func() (string, error) {
-		hasGit := func(p string) bool {
-			// ya, i know.
-			return func() error { _, err := os.Stat(path.Join(p, ".git")); return err }() == nil
-		}
-		for cur := path.Clean(wd); ; cur = path.Dir(cur) {
-			switch {
-			case hasGit(cur):
-				return cur, nil
-			case cur == "/":
-				return "", fmt.Errorf(".git directory not found")
-			}
-		}
+	hasGit := func(p string) bool {
+		// ya, i know.  i was just dinking around here.  will simplify this later.
+		return func() error { _, err := os.Stat(path.Join(p, ".git")); return err }() == nil
 	}
 
-	dir, err := findGit()
+	var cur string
+	for cur = path.Clean(wd); !hasGit(cur) && cur != "/"; cur = path.Dir(cur) {
+	}
+
+	head, err := os.Open(path.Join(cur, "./.git/HEAD"))
 	if err != nil {
-		return err
-	}
-
-	head, err := os.Open(path.Join(dir, "./.git/HEAD"))
-	if err != nil {
-		return err
+		return
 	}
 	defer head.Close()
 
 	var ref string
 	matches, err := fmt.Fscanf(head, "ref: %s", &ref)
-	if err != nil {
-		return err
-	}
-	if matches != 1 {
-		return fmt.Errorf("could not parse HEAD file")
+	if err != nil || matches != 1 {
+		return
 	}
 
-	if _, err := fmt.Fprintf(w, "%s", strings.TrimPrefix(ref, "refs/heads/")); err != nil {
-		return err
-	}
-
-	return nil
+	fmt.Fprintf(w, "%s", strings.TrimPrefix(ref, "refs/heads/"))
 }
 
 func main() {
 	prompt := strings.Builder{}
+
 	gitinfo(&prompt)
 
 	if prompt.Len() == 0 {
